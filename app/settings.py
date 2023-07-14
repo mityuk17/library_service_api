8214eeb627799c8d5ac748933f01d106260747e3
mityuk17 <mityukov.17@gmail.com>
2023-07-14 15:23:22 +0300

    -

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,7 @@ import os
 """config for database"""
 DB_USER = os.environ.get('DB_USER', 'postgres')
 DB_PASSWORD = os.environ.get('DB_PASSWORD', 'password')
-DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_HOST = os.environ.get('DB_HOST', '0.0.0.0')
 DB_NAME = os.environ.get('DB_NAME', 'postgres')
 DB_PORT = os.environ.get('DB_PORT', default=5432)
 postgre_url = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
